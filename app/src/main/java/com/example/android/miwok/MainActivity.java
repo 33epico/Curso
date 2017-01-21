5ee857e2debbe2c79ce4672a6b300060ca5fd7b9
Eduardo <33epico@gmail.com>
2017-01-21 18:31:10 +0100

    Probando
    Test

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,11 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,4 +30,23 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
     }
+
+    TextView colors = (TextView) findViewById(R.id.colors);
+
+    // Set a click listener on that View
+    colors.setOnClickListener  (new OnClickListener() {
+        // The code in this method will be executed when the colors category is clicked on.
+        @Override
+        public void onClick (View view){
+            // Create a new intent to open the {@link ColorsActivity}
+            Intent colorsIntent = new Intent(MainActivity.this, Colores.class);
+
+            // Start the new activity
+            startActivity(colorsIntent);
+        }
+    }
+
+    );
+
+
 }
---
+++ app/src/main/java/com/example/android/miwok/Numbers.java
@@ -0,0 +1,15 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Numbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("NUMEROS");
+        setContentView(R.layout.activity_numbers);
+    }
+
+}
